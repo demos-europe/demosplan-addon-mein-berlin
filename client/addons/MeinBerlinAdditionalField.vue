a45a829474e73c2b767d8ca20c06030653d38c3c
Velimir Müller <mueller@demos-deutschland.de>
2024-11-25 11:24:38 +0100

    feat(refs BEAA2-10): replace dp-input with dp-select
    - also add the selectable options in the data props

--- client/addons/MeinBerlinAdditionalField.vue
+++ client/addons/MeinBerlinAdditionalField.vue
@@ -1,26 +1,26 @@
 <template>
-  <dp-input
+  <dp-select
     id="addonAdditionalField"
+    name="addonAdditionalField"
     :data-cy="`${resourceType}:field`"
     :label="{
       text: label,
       hint: hint,
       tooltip: tooltip
     }"
-    :required="required || (Boolean(initValue) && !isValueRemovable)"
+    :options="options"
     v-model="currentValue"
-    @blur="$emit('addonEvent:emit', { name: 'blur', payload: addonPayload })"
-    @focus="handleFocus" />
+    @selected="$emit('addonEvent:emit', { name: 'blur', payload: addonPayload })" />
 </template>
 
 <script>
-import { dpApi, DpInput} from '@demos-europe/demosplan-ui'
+import { dpApi, DpSelect } from '@demos-europe/demosplan-ui'
 
 export default {
   name: 'AddonAdditionalField',
 
   components: {
-    DpInput
+    DpSelect
   },
 
   props: {
@@ -55,6 +55,21 @@ export default {
       initValue: '',
       item: null,
       list: null,
+      options: [
+        { label: 'Senatsverwaltung für Stadtentwicklung, Bauen und Wohnen', value: '14' },
+        { label: 'Bezirksamt Charlottenburg-Wilmersdorf', value: '27' },
+        { label: 'Bezirksamt Friedrichshain-Kreuzberg', value: '28' },
+        { label: 'Bezirksamt Lichtenberg ', value: '29' },
+        { label: 'Bezirksamt Marzahn-Hellersdorf ', value: '25' },
+        { label: 'Bezirksamt Mitte', value: '16' },
+        { label: 'Bezirksamt Neukölln', value: '30' },
+        { label: 'Bezirksamt Pankow', value: '20' },
+        { label: 'Bezirksamt Reinickendorf', value: '31' },
+        { label: 'Bezirksamt Spandau', value: '26' },
+        { label: 'Bezirksamt Steglitz-Zehlendorf', value: '32' },
+        { label: 'Bezirksamt Tempelhof-Schöneberg', value: '24' },
+        { label: 'Bezirksamt Treptow-Köpenick', value: '15' }
+      ],
       relationshipKeyMapping: {
         'orga': {
           attribute: 'meinBerlinOrganisationId',
@@ -110,10 +125,10 @@ export default {
   },
 
   methods: {
-    fetchResourceList () {
-      const url = Routing.generate('api_resource_list', { resourceType: this.resourceType })
+    fetchResourceList() {
+      const url = Routing.generate('api_resource_list', {resourceType: this.resourceType})
 
-      return dpApi.get(url, { include: [this.relationshipKey].join() })
+      return dpApi.get(url, {include: [this.relationshipKey].join()})
         .then(response => {
           this.list = response.data.data.map(item => {
             return {
@@ -126,20 +141,12 @@ export default {
         .catch(err => console.error(err))
     },
 
-    getItemByRelationshipId () {
+    getItemByRelationshipId() {
       this.item = Object.values(this.list).find(el => el.relationships[this.relationshipKey].data.id === this.relationshipId) || null
 
       if (this.item) {
         this.currentValue = this.item.attributes[this.attribute]
         this.initValue = this.item.attributes[this.attribute]
-      }
-    },
-
-    handleFocus () {
-      const input = document.getElementById('addonAdditionalField')
-
-      if (input.classList.contains('is-invalid')) {
-        input.classList.remove('is-invalid')
       }
     }
   },
