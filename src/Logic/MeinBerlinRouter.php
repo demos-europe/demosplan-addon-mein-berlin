64484dee5d66ff0d3519d8cafcdf1a8f86d6eb9f
Saeed Jalalifar <jalalifar@demos-deutschland.de>
2024-12-02 14:01:51 +0100

    feature (ref BEAA2-10): remove reimolemented core feature

--- src/Logic/MeinBerlinRouter.php
+++ src/Logic/MeinBerlinRouter.php
@@ -11,22 +11,40 @@ declare(strict_types=1);
 
 namespace DemosEurope\DemosplanAddon\DemosMeinBerlin\Logic;
 
+use DemosEurope\DemosplanAddon\DemosMeinBerlin\Exception\MeinBerlinRssFeedException;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+use Symfony\Component\Routing\Generator\UrlGenerator;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Routing\RequestContext;
+use Symfony\Component\Routing\Route;
+use Symfony\Component\Routing\RouteCollection;
 use Symfony\Component\Routing\RouterInterface;
+use Webmozart\Assert\Assert;
 
 class MeinBerlinRouter
 {
+    private const RSS_FEED = 'rss_feed';
+
+    private UrlGeneratorInterface $generator;
     private RouterInterface $router;
 
-    public function __construct(RouterInterface $router)
+    public function __construct(ParameterBagInterface $parameterBag, RouterInterface $router)
     {
+        $baseUrl = $parameterBag->get('mein_berlin_api_baseurl') ??
+            throw MeinBerlinRssFeedException::missingParameter('mein_berlin_api_baseurl');
+        Assert::string($baseUrl);
+
+        $routes = new RouteCollection();
+        $routes->add(self::RSS_FEED, new Route('/api/{organisationId}/rss-feed'));
+
+        $this->generator = new UrlGenerator($routes, RequestContext::fromUri($baseUrl));
         $this->router = $router;
     }
 
     public function rssFeed(string $organisationId): string
     {
-        return $this->router->generate(
-            'rss_feed',
+        return $this->generator->generate(
+            self::RSS_FEED,
             ['organisationId' => $organisationId],
             UrlGeneratorInterface::ABSOLUTE_URL
         );
